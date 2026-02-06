7429adc4e93637dad6237f30de5e5b15cfb65c5a
Latifan Nurdiansyah <latifannurdiansyah02@gmail.com>
2026-02-06 16:32:08 +0700

    .

--- gps-tracker-public/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <a href="/dashboard" className="text-blue-500 text-xl">
-        Open GPS Dashboard
-      </a>
-    </div>
-  )
-}
